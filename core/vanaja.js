84a8ab4aafff2905dfbc67b4dd3f9f8be72178d1
Willi Thiel <ni-c@ni-c.de>
2012-07-29 03:29:52 +0200

    Added attack animation

--- core/vanaja.js
+++ core/vanaja.js
@@ -3,6 +3,19 @@ RPGJS.loadPath = "rpgjs/core/";
 RPGJS.load(function() {
 	rpg = new Rpg("canvas_rpg");
 	rpg.setVolumeAudio(1);
+				
+	/* Player attack animation */
+	rpg.addAction('myattack', {
+		action: 'attack',
+		suffix_motion: ['_Attack'],
+		duration_motion: 1,
+		block_movement: true,
+		wait_finish: 5,
+		speed: 10,
+		keypress: [Input.Ctrl]
+	});
+
+	/* Load the map Wood */
 	rpg.loadMap('Wood', {
 		tileset: 'Swamp.png',
 		autotiles: ['Swamp1.png', 'Swamp2.png', 'Swamp3.png', 'Swamp4.png', 'Swamp5.png'],
@@ -12,7 +25,12 @@ RPGJS.load(function() {
 			speed: 10,
 			x: 4, 
 			y: 4, 
-			filename: 'Ark.png'
+			filename: 'Ark.png',
+			actionBattle: {
+				hp_max: 200,
+				actions: ['myattack']
+			},
+			actions: ['myattack']
 		}
 	}, function () { 
 		rpg.player.setTypeMove("real");
